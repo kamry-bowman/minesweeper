91a2d2c0abd2aaee3d8ca8956e859994d2b90eab
Kamry Bowman <kamry.bowman@gmail.com>
2019-04-15 17:23:09 -0600

    Displaying game board

---
+++ mines.py
@@ -0,0 +1,93 @@
+from random import random
+from collections import namedtuple
+
+Pos = namedtuple("Pos", ("row", "col"))
+
+
+class Game:
+    def __init__(self, size=8):
+        self.board = self.make_grid(size)
+        self.size = size
+        self.result = None
+
+    @staticmethod
+    def make_grid(size):
+        rows = []
+        for r in range(0, size):
+            row = []
+            for c in range(0, size):
+                cell = Cell(Pos(r, c))
+                if random() > .7:
+                    cell.mined = True
+                row.append(cell)
+            rows.append(row)
+        return rows
+
+    def print_board(self):
+        print('   ' + ' '.join([str(i) for i in range(self.size)]))
+        print('   ' + ' '.join(['-' for i in range(self.size)]))
+        for row_index in range(self.size):
+            strs = [str(row_index)+"|"]
+            for cell in self.board[row_index]:
+                strs.append(self.draw_cell(cell))
+            print(" ".join(strs))
+
+    def draw_cell(self, cell):
+        if cell.clicked == False:
+            return '?'
+
+        elif cell.surrounding is None:
+            cell.surrounding = self.count_surrounding_mines(cell.pos)
+
+        return str(cell.surrounding)
+
+    def count_surrounding_mines(self, pos):
+        mine_count = 0
+        rowIndex = pos.row
+        colIndex = pos.col
+
+        for row_delta in range(-1, 2, 1):
+            for col_delta in range(-1, 2, 1):
+                r = rowIndex + row_delta
+                c = colIndex + col_delta
+                is_not_self = not (r == rowIndex and c == colIndex)
+                if self.pos_is_on_board(r, c) and is_not_self and self.board[r][c].mined:
+                    mine_count += 1
+
+        return mine_count
+
+    def pos_is_on_board(self, row, col):
+        return row >= 0 and row < self.size and col >= 0 and col < self.size
+
+    def handle_move(self, row, col):
+        cell = self.board[row][col]
+        if cell.mined:
+            self.result = "You lost."
+        else:
+            cell.clicked = True
+
+
+class Cell:
+    def __init__(self, pos):
+        self.clicked = False
+        self.mined = False
+        self.surrounding = None
+
+
+if __name__ == "__main__":
+    game = Game(4)
+    while game.result is None:
+        game.print_board()
+        choice = input("Enter a move in format row,col:\n")
+        parsed = choice.split(",")
+        if len(parsed) != 2:
+            print(
+                f"Read {parsed} from {choice}, please enter in format row,col")
+        else:
+            row = parsed[0]
+            col = parsed[1]
+            if not game.pos_is_on_board(row, col):
+                print(
+                    f"Row {row} and col {col} are not on size {game.size} board")
+            else:
+                game.handle_move(row, col)
