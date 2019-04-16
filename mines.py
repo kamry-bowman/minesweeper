6c367598bb8bfa189d474d63b514de6b6a48b04c
Kamry Bowman <kamry.bowman@gmail.com>
2019-04-15 18:23:13 -0600

    functioning game

--- mines.py
+++ mines.py
@@ -6,12 +6,12 @@ Pos = namedtuple("Pos", ("row", "col"))
 
 class Game:
     def __init__(self, size=8):
-        self.board = self.make_grid(size)
         self.size = size
         self.result = None
+        self.flags_remaining = 0
+        self.mines = 0
+        self.mines_found = 0
 
-    @staticmethod
-    def make_grid(size):
         rows = []
         for r in range(0, size):
             row = []
@@ -19,11 +19,15 @@ class Game:
                 cell = Cell(Pos(r, c))
                 if random() > .7:
                     cell.mined = True
+                    self.flags_remaining += 1
+                    self.mines += 1
                 row.append(cell)
             rows.append(row)
-        return rows
+
+        self.board = rows
 
     def print_board(self):
+        print(f"Flags remaining: {self.flags_remaining}")
         print('   ' + ' '.join([str(i) for i in range(self.size)]))
         print('   ' + ' '.join(['-' for i in range(self.size)]))
         for row_index in range(self.size):
@@ -33,11 +37,11 @@ class Game:
             print(" ".join(strs))
 
     def draw_cell(self, cell):
+        if cell.flagged == True:
+            return "f"
+
         if cell.clicked == False:
             return '?'
-
-        elif cell.surrounding is None:
-            cell.surrounding = self.count_surrounding_mines(cell.pos)
 
         return str(cell.surrounding)
 
@@ -65,29 +69,74 @@ class Game:
             self.result = "You lost."
         else:
             cell.clicked = True
+            cell.surrounding = self.count_surrounding_mines(cell.pos)
+
+    def handle_flag(self, row, col):
+        cell = self.board[row][col]
+        if cell.flagged:
+            self.flags_remaining += 1
+            cell.flagged = False
+            # if they unflag a flagged mine, update gamestate
+            if cell.mined:
+                self.mines_found -= 1
+        else:
+            self.flags_remaining -= 1
+            cell.flagged = True
+            # if they flag a mine, update gamestate
+            if cell.mined:
+                self.mines_found += 1
+
+    def update_game_result_if_victory(self):
+        if self.mines_found == self.mines:
+            self.result = "Congratulations, you won!"
 
 
 class Cell:
     def __init__(self, pos):
         self.clicked = False
         self.mined = False
+        self.flagged = False
         self.surrounding = None
+        self.pos = pos
 
 
 if __name__ == "__main__":
     game = Game(4)
     while game.result is None:
+        flag = False
         game.print_board()
-        choice = input("Enter a move in format row,col:\n")
+        choice = input(
+            "Enter a move in format <row>,<col>:\nTo toggle a flag on a space, enter f:\n")
         parsed = choice.split(",")
+        # check for flag
+        if len(parsed) == 1 and parsed[0] == "f":
+            if game.flags_remaining == 0:
+                print("You have no flags remaining")
+                continue
+            flag = True
+            choice = input("Enter space to flag in format <row>,<col>:\n")
+            parsed = choice.split(",")
+        # else handle move
         if len(parsed) != 2:
             print(
                 f"Read {parsed} from {choice}, please enter in format row,col")
         else:
-            row = parsed[0]
-            col = parsed[1]
+            try:
+                row = int(parsed[0])
+                col = int(parsed[1])
+            except ValueError:
+                print(f"Could not parse {parsed}")
+                continue
             if not game.pos_is_on_board(row, col):
                 print(
                     f"Row {row} and col {col} are not on size {game.size} board")
             else:
-                game.handle_move(row, col)
+                if not flag:
+                    game.handle_move(row, col)
+                else:
+                    game.handle_flag(row, col)
+
+        if game.result is None:
+            game.update_game_result_if_victory()
+
+    print(game.result)
